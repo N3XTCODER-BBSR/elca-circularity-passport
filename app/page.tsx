9f7b081cca95351fd232efa4ca75728d6f2cf697
Daniel Spaude <daniel@spaude.de>
2024-07-01 17:58:14 +0200

    small content adaptions to starter page

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,17 @@ import { Button } from "components/Button/Button"
 import { LP_GRID_ITEMS } from "lp-items"
 
 export const metadata: Metadata = {
-  title: "Next.js Enterprise Boilerplate",
+  title: "Building Resource Passport",
   twitter: {
     card: "summary_large_image",
   },
   openGraph: {
-    url: "https://next-enterprise.vercel.app/",
+    url: "https://digitalbuildingpassport.app/",
     images: [
       {
         width: 1200,
         height: 630,
-        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png",
+        url: "",
       },
     ],
   },
@@ -26,7 +26,7 @@ export default function Web() {
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
             <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
-              Next.js Enterprise Boilerplate
+            Building Resource Passport
             </h1>
             <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
               Jumpstart your enterprise project with our feature-packed, high-performance Next.js boilerplate!
