cec135961223c68834a16d0d58ddd429b348167e
Daniel Spaude <daniel@spaude.de>
2024-07-05 00:00:28 +0200

    prisma deploy for scalingo (WIP) - add missing script falg; started with calling seeders

--- devops/scalingoPostbuild.sh
+++ devops/scalingoPostbuild.sh
@@ -4,7 +4,7 @@ RUN_SEEDERS_ON_DEPLOY=${RUN_SEEDERS_ON_DEPLOY:-0}
 
 set -eux -o pipefail
 
-npx prisma prisma:deploy
+npx prisma migrate deploy
 
 if [ "${RUN_SEEDERS_ON_DEPLOY}" == 1 ]; then
   echo "Seeding database"
