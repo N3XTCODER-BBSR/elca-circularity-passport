77b6e07346726c8327aba69a01d5a1bed60c294a
Daniel Spaude <daniel@spaude.de>
2024-07-09 11:53:43 +0200

    update seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -13,12 +13,23 @@ async function main() {
       url: "",
     },
     buildingBaseData: {
-      buildingStructureId: "UUID-123",
-      address: "Mariendorfer Weg 28, 12051 Berlin",
-      buildingYear: 2024,
-      buildingType: "Bürogebäude",
-      numberOfFloors: 8,
-      plotArea: "10000 m²",
+      buildingStructureId: "014061-002-00079/0004",
+      address: "Musterstrasse 3, 10203 Berlin",
+      buildingYear: 2025,
+      buildingType: "9890 - sonstige Gebäude",
+      numberOfFloors: 3,
+      plotArea: 1500, // Converting plotArea to a number
+      nrf: 1067.0,
+      bgf: 1210.0,
+      bri: 3327.5,
+      propertyArea: 1500,
+      sealedPropertyAreaProportion: 0.822,
+      totalBuildingMass: 9900000,
+      dataQuality: "Estimiert",
+      queryPlanningDocumentsAvailable: false,
+      planningDocuments: "",
+      hazardousSubstanceReportAvailable: false,
+      assessments: "",
     },
     buildingComponents: [
       {
@@ -26,30 +37,123 @@ async function main() {
         name: "Fassade",
         layers: [
           {
-            mass: 1000,
-            material: {
-              uuid: "UUID-12345",
-              description: "Beton C30/37",
-              materialReferenceDatabase: {
-                name: "Ökobau.dat",
-                version: "2021",
-                url: "https://www.oekobaudat.de/",
-              },
-            },
+            buildingId: "3444a12c-8da3-42b1-88ee-a5d65ade1785",
+            lnr: 0,
+            floor: "",
+            room: "",
+            componentNumber: "7548707",
+            componentName: "Kellerboden",
+            costGroupDIN276: "324",
+            amount: 252,
+            componentGeometry: "m2",
+            mass: 17.64,
+            materialDescription: "PVC Plastisol",
+            materialDatabase: "OBD_2020_II_A1",
+            serviceLife: 50,
+            technicalServiceLife: 50,
+            uuidProduct: "",
+            productDescription: "",
+            manufacturerName: "",
+            proofDocument: "",
+            versionDate: "",
+            wasteCode: "10106",
             circularity: {
               interferingSubstances: [
+                // Assuming these are static, can be adjusted as needed
                 {
-                  name: "Stahl",
-                  description: "Stahl",
-                  category: "Metall",
-                  subCategory: "Stahl",
-                  resource: "Metall",
-                  resourceCategory: "Metall",
-                  resourceSubCategory: "Stahl",
-                  resourceUnit: "kg",
-                  resourceUnitValue: 1,
-                  resourceUnitConversion: 1,
-                  resourceUnitConversionValue: 1,
+                  // Sample interfering substance
+                },
+              ],
+              circularityIndex: {
+                points: 10,
+                className: "A",
+              },
+            },
+            pollutants: {
+              // Define properties here when they are clear
+            },
+            ressources: {
+              rmiMineralKg: "1000",
+              rmiMetallic: "1000",
+              rmiFossil: "1000",
+              rmiForestry: "1000",
+              rmiAgrar: "1000",
+              rmiAqua: "1000",
+            },
+            serviceLifeYear: 50,
+          },
+          {
+            buildingId: "3444a12c-8da3-42b1-88ee-a5d65ade1786",
+            lnr: 1,
+            floor: "",
+            room: "",
+            componentNumber: "7548707",
+            componentName: "Kellerboden",
+            costGroupDIN276: "324",
+            amount: 252,
+            componentGeometry: "m2",
+            mass: 24192,
+            materialDescription: "Zementestrich",
+            materialDatabase: "OBD_2020_II_A2",
+            serviceLife: 50,
+            technicalServiceLife: 50,
+            uuidProduct: "",
+            productDescription: "",
+            manufacturerName: "",
+            proofDocument: "",
+            versionDate: "",
+            wasteCode: "10105",
+            circularity: {
+              interferingSubstances: [
+                // Assuming these are static, can be adjusted as needed
+                {
+                  // Sample interfering substance
+                },
+              ],
+              circularityIndex: {
+                points: 10,
+                className: "A",
+              },
+            },
+            pollutants: {
+              // Define properties here when they are clear
+            },
+            ressources: {
+              rmiMineralKg: "1000",
+              rmiMetallic: "1000",
+              rmiFossil: "1000",
+              rmiForestry: "1000",
+              rmiAgrar: "1000",
+              rmiAqua: "1000",
+            },
+            serviceLifeYear: 50,
+          },
+          {
+            buildingId: "3444a12c-8da3-42b1-88ee-a5d65ade1787",
+            lnr: 2,
+            floor: "",
+            room: "",
+            componentNumber: "7548707",
+            componentName: "Kellerboden",
+            costGroupDIN276: "324",
+            amount: 252,
+            componentGeometry: "m2",
+            mass: 11869.2,
+            materialDescription: "Bewehrungsstahl",
+            materialDatabase: "OBD_2020_II_A3",
+            serviceLife: 50,
+            technicalServiceLife: 50,
+            uuidProduct: "",
+            productDescription: "",
+            manufacturerName: "",
+            proofDocument: "",
+            versionDate: "",
+            wasteCode: "10113",
+            circularity: {
+              interferingSubstances: [
+                // Assuming these are static, can be adjusted as needed
+                {
+                  // Sample interfering substance
                 },
               ],
               circularityIndex: {
