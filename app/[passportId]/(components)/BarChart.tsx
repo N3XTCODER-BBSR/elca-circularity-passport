56a5e7bb4b4c76259755207124035f7f060a3e99
Daniel Spaude <daniel@spaude.de>
2024-07-15 22:08:11 +0200

    module 1 - work on chart - WIP

--- app/[passportId]/(components)/BarChart.tsx
+++ app/[passportId]/(components)/BarChart.tsx
@@ -7,7 +7,7 @@ import { BarDatum, ResponsiveBar } from "@nivo/bar"
 // no chart will be rendered.
 // website examples showcase many properties,
 // you'll often use just a few of them.
-const BarChart = ({ data, indexBy, keys }: { data: BarDatum[]; indexBy: string; keys: string[] }) => (
+const BarChart = ({ data, indexBy, keys, labelKey }: { data: BarDatum[]; indexBy: string; keys: string[], labelKey: string }) => (
   <>
     <ResponsiveBar
       data={data}
@@ -30,7 +30,6 @@ const BarChart = ({ data, indexBy, keys }: { data: BarDatum[]; indexBy: string; 
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: "mass",
         legendPosition: "middle",
         legendOffset: 32,
         truncateTickAt: 0,
@@ -39,45 +38,49 @@ const BarChart = ({ data, indexBy, keys }: { data: BarDatum[]; indexBy: string; 
         tickSize: 5,
         tickPadding: 5,
         tickRotation: 0,
-        legend: "Bauteilkategorien",
         legendPosition: "middle",
         legendOffset: -40,
         truncateTickAt: 0,
       }}
+      tooltip={() => null}
       totalsOffset={9}
+      enableLabel={true}
+      label={(e) => `${e.data[labelKey]}`}
+      // labelPosition="end"
       labelSkipWidth={12}
       labelSkipHeight={12}
-      labelTextColor={{
-        from: "color",
-        modifiers: [["darker", 1.6]],
-      }}
-    //   legends={[
-    //     {
-    //       dataFrom: "keys",
-    //       anchor: "bottom-right",
-    //       direction: "column",
-    //       justify: false,
-    //       translateX: 120,
-    //       translateY: 0,
-    //       itemsSpacing: 2,
-    //       itemWidth: 100,
-    //       itemHeight: 20,
-    //       itemDirection: "left-to-right",
-    //       itemOpacity: 0.85,
-    //       symbolSize: 20,
-    //       effects: [
-    //         {
-    //           on: "hover",
-    //           style: {
-    //             itemOpacity: 1,
-    //           },
-    //         },
-    //       ],
-    //     },
-    //   ]}
+      labelTextColor={"white"}
+      // labelTextColor={{
+      //   from: "color",
+      //   modifiers: [["darker", 1.6]],
+      // }}
+      //   legends={[
+      //     {
+      //       dataFrom: "keys",
+      //       anchor: "bottom-right",
+      //       direction: "column",
+      //       justify: false,
+      //       translateX: 120,
+      //       translateY: 0,
+      //       itemsSpacing: 2,
+      //       itemWidth: 100,
+      //       itemHeight: 20,
+      //       itemDirection: "left-to-right",
+      //       itemOpacity: 0.85,
+      //       symbolSize: 20,
+      //       effects: [
+      //         {
+      //           on: "hover",
+      //           style: {
+      //             itemOpacity: 1,
+      //           },
+      //         },
+      //       ],
+      //     },
+      //   ]}
       role="application"
-    //   ariaLabel="Nivo bar chart demo"
-    //   barAriaLabel={(e) => e.id + ": " + e.formattedValue + " in country: " + e.indexValue}
+      //   ariaLabel="Nivo bar chart demo"
+      //   barAriaLabel={(e) => e.id + ": " + e.formattedValue + " in country: " + e.indexValue}
     />
   </>
 )
--- app/[passportId]/(components)/tabs/modules/Materials.tsx
+++ app/[passportId]/(components)/tabs/modules/Materials.tsx
@@ -4,78 +4,80 @@ import { BuildingComponent } from "utils/zod/passportSchema"
 import BarChart from "../../BarChart"
 
 interface AggregatedData {
-  costGroupCategory: number;
-  categoryName: string;
-  aggregatedMass: number;
+  costGroupCategory: number
+  categoryName: string
+  aggregatedMass: number
+  aggregatedMassPercentage: number
+  label: string
 }
 
-
 const Materials = ({ buildingComponents }: { buildingComponents: BuildingComponent[] }) => {
-  console.log('buildingComponents', buildingComponents)
+  console.log("buildingComponents", buildingComponents)
 
+  function aggregateData(buildingComponents: BuildingComponent[]): AggregatedData[] {
+    // Initialize a map to store aggregated data
+    const aggregationMap = new Map<number, { categoryName: string; aggregatedMass: number }>()
 
-function aggregateData(buildingComponents: BuildingComponent[]): AggregatedData[] {
-  // Initialize a map to store aggregated data
-  const aggregationMap = new Map<number, { categoryName: string, aggregatedMass: number }>();
-
-  buildingComponents.forEach(component => {
-      const { costGroupCategory, categoryName, layers } = component;
+    buildingComponents.forEach((component) => {
+      const { costGroupCategory, categoryName, layers } = component
 
       // Calculate the total mass for this component
-      const totalMass = layers.reduce((sum, layer) => sum + layer.mass, 0);
+      const totalMass = layers.reduce((sum, layer) => sum + layer.mass, 0)
 
       // Check if the costGroupCategory already exists in the map
       if (aggregationMap.has(costGroupCategory)) {
-          // Update the existing entry
-          aggregationMap.get(costGroupCategory)!.aggregatedMass += totalMass;
+        // Update the existing entry
+        aggregationMap.get(costGroupCategory)!.aggregatedMass += totalMass
       } else {
-          // Create a new entry
-          aggregationMap.set(costGroupCategory, {
-              categoryName,
-              aggregatedMass: totalMass
-          });
+        // Create a new entry
+        aggregationMap.set(costGroupCategory, {
+          categoryName,
+          aggregatedMass: totalMass,
+        })
       }
-  });
+    })
 
-  // Convert the map to an array of AggregatedData
-  return Array.from(aggregationMap, ([costGroupCategory, { categoryName, aggregatedMass }]) => ({
+    // Convert the map to an array of AggregatedData
+    const aggretatedDataArr = Array.from(aggregationMap, ([costGroupCategory, { categoryName, aggregatedMass }]) => ({
       costGroupCategory,
       categoryName,
-      aggregatedMass
-  }));
-}
+      aggregatedMass,
+    }))
 
-const aggregatedData = aggregateData(buildingComponents);
-console.log(aggregatedData);
+    const totalMass = aggretatedDataArr.reduce((sum, { aggregatedMass }) => sum + aggregatedMass, 0)
 
+    const aggregatedDataArrWithPercentageAndLabel = aggretatedDataArr.map((data) => {
+      const aggregatedMassPercentage = (data.aggregatedMass / totalMass) * 100
+      return {
+        ...data,
+        aggregatedMassPercentage,
+        label: `${aggregatedMassPercentage.toFixed(2)}% (${data.aggregatedMass.toFixed(2)} t)`,
+      }
+    })
+
+    const aggretatedDataWithPercentageSorted = aggregatedDataArrWithPercentageAndLabel.sort(
+      (a, b) => a.aggregatedMass - b.aggregatedMass
+    )
+
+    console.log("aggretatedDataWithPercentageSorted", aggretatedDataWithPercentageSorted)
+
+    return aggretatedDataWithPercentageSorted
+  }
+
+  const aggregatedData = aggregateData(buildingComponents)
+  console.log(aggregatedData)
 
   // TODO: Change this so keys is not showing the costGroupCategoryID (cryptic) but the respective categoryName
   // const keys = [...buildingComponents.map((component) => component.categoryName)]
-  const keys = ['mass']
+  const keys = ["aggregatedMassPercentage"]
 
   const chartData = aggregatedData.map((data) => ({
     categoryName: data.categoryName,
     mass: data.aggregatedMass,
-  }));
-
-  console.log('chartData', chartData)
-    
-
-  // const chartData = buildingComponents.map((component) => {
-
-  // const chartData = [{
-  //   costGroupCategoryID: 320, 
-  //   mass: 1423,
-  // }]
-  
-  // const keys = ["hot dog", "burger", "sandwich", "kebab"]
-
-  // const chartData = [
-  //   {
-  //     categoryName: "ABC",
-  //     mass: 171,
-  //   },
-  // ]
+    aggregatedMassPercentage: data.aggregatedMassPercentage,
+    // label is of format xx% (abc t)
+    label: data.label,
+  }))
 
   return (
     <div>
@@ -86,7 +88,7 @@ console.log(aggregatedData);
         Materialien
       </h3>
       <div className="h-[300px]">
-        <BarChart data={chartData} indexBy={"categoryName"} keys={keys} />
+        <BarChart data={chartData} indexBy={"categoryName"} keys={keys} labelKey="label" />
       </div>
     </div>
   )
