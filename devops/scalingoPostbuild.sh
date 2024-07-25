1811b60d3d3f2c407a12b181b9ca883f3628cdf9
Daniel Spaude <daniel@spaude.de>
2024-07-25 12:40:52 +0200

    add 'build' command from scalingoPostbuild again (#17)

--- devops/scalingoPostbuild.sh
+++ devops/scalingoPostbuild.sh
@@ -4,7 +4,7 @@ RUN_SEEDERS_ON_DEPLOY=${RUN_SEEDERS_ON_DEPLOY:-0}
 
 set -eux -o pipefail
 
-# npx next build
+npx next build
 npx prisma migrate deploy
 
 if [ "${RUN_SEEDERS_ON_DEPLOY}" == 1 ]; then
