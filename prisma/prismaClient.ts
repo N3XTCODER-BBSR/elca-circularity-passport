bd3c280fc687480134817831c3f39f231b65d238
Nikolas <niko.heidner@gmail.com>
2024-11-13 17:34:22 +0100

    feat: create prisma client for legacy database including all schemas

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,15 +1,25 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "./generated/client"
+import { PrismaClient as PrismaLegacyClient } from "./generated/client-legacy"
 
 const prismaClientSingleton = () => {
   return new PrismaClient()
 }
 
+const prismaLegacyClientSingleton = () => {
+  return new PrismaLegacyClient()
+}
+
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>
+  prismaLegacyGlobal: ReturnType<typeof prismaLegacyClientSingleton>
 } & typeof global
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prismaLegacy = globalThis.prismaLegacyGlobal ?? prismaLegacyClientSingleton()
 
-export default prisma
+export { prisma, prismaLegacy }
 
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prismaGlobal = prisma
+  globalThis.prismaLegacyGlobal = prismaLegacy
+}
