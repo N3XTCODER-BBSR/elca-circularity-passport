5963b03d7b52e391a7ae1255a9d415882c2e18aa
Nikolas <niko.heidner@gmail.com>
2024-11-13 19:01:19 +0100

    fix: don't build app and seed twice

--- devops/scalingoPostbuild.sh
+++ devops/scalingoPostbuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-RUN_SEEDERS_ON_DEPLOY=${RUN_SEEDERS_ON_DEPLOY:-0}
+# RUN_SEEDERS_ON_DEPLOY=${RUN_SEEDERS_ON_DEPLOY:-0}
 
 set -eux -o pipefail
 
@@ -9,9 +9,9 @@ echo "NEXTAUTH_URL=${NEXTAUTH_URL}" >> .env
 echo "NEXTAUTH_URL=${NEXTAUTH_URL}"
 
 npx prisma migrate reset --force
-npx next build
+# npx next build
 
-if [ "${RUN_SEEDERS_ON_DEPLOY}" == "1" ] || [ "${RUN_SEEDERS_ON_DEPLOY}" == "true" ]; then
-  echo "Seeding database"
-  npx prisma db seed
-fi
+# if [ "${RUN_SEEDERS_ON_DEPLOY}" == "1" ] || [ "${RUN_SEEDERS_ON_DEPLOY}" == "true" ]; then
+#   echo "Seeding database"
+#   npx prisma db seed
+# fi
