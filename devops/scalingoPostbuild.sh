c01e82f0822d1de4a440368f8f738466761fe554
Nikolas <niko.heidner@gmail.com>
2024-11-14 08:38:20 +0100

    fix: clean up scalingo Postbuild script

--- devops/scalingoPostbuild.sh
+++ devops/scalingoPostbuild.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
-# RUN_SEEDERS_ON_DEPLOY=${RUN_SEEDERS_ON_DEPLOY:-0}
-
 set -eux -o pipefail
 
 export NEXTAUTH_URL="https://${APP}.${REGION_NAME}.scalingo.io"
 echo "NEXTAUTH_URL=${NEXTAUTH_URL}" >> .env
 echo "NEXTAUTH_URL=${NEXTAUTH_URL}"
 
-npx prisma migrate reset --force
-# npx next build
+# TODO: when running in real production, database should only be migrated, not reset and seeded
 
-# if [ "${RUN_SEEDERS_ON_DEPLOY}" == "1" ] || [ "${RUN_SEEDERS_ON_DEPLOY}" == "true" ]; then
-#   echo "Seeding database"
-#   npx prisma db seed
-# fi
+# deletes the database, runs the migrations and seeds the database again
+npx prisma migrate reset --force
