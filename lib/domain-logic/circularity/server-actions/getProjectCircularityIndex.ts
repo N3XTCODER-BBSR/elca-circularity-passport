14f05c79bb9bfe6ca67b7477eb1b3de5c1cfa79c
Daniel Spaude <daniel@spaude.de>
2024-11-27 17:16:13 +0100

    WIP

--- lib/domain-logic/circularity/server-actions/getProjectCircularityIndex.ts
+++ lib/domain-logic/circularity/server-actions/getProjectCircularityIndex.ts
@@ -6,16 +6,11 @@ import {
   TBaustoffProductData,
   UserEnrichedProductDataWithDisturbingSubstanceSelection,
 } from "lib/domain-logic/types/domain-types"
-import { query } from "lib/elca-legacy-db"
-import { prisma } from "prisma/prismaClient"
-import { Prisma, TBs_OekobaudatMapping, UserEnrichedProductData } from "../../../../prisma/generated/client"
-import { calculateEolDataByEolCateogryData } from "../utils/calculateEolDataByEolCateogryData"
 import { CalculateCircularityDataForLayerReturnType } from "../utils/calculate-circularity-data-for-layer"
 
 type ProjectCircularityIndexData = {
   projectId: string
   projectName: string
-  // totalCircularityIndex: number
   components: ElcaElementWithComponents<CalculateCircularityDataForLayerReturnType>[]
 }
 
