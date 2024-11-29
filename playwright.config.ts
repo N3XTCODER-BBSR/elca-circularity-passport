1da0978cc804148be872094b42ade7a44ae3a7e5
Nikolas <niko.heidner@gmail.com>
2024-11-29 19:12:38 +0100

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,7 +74,7 @@ export default defineConfig({
     command: process.env.CI ? "yarn start:docker" : "yarn dev", // TODO: yarn dev only starts the nextjs server, not the database servers
     url: `http://127.0.0.1:${process.env.CI ? "3005" : "3000"}`,
     timeout: process.env.CI ? 600 * 1000 : 60 * 1000,
-    // reuseExistingServer: true,
+    reuseExistingServer: true,
     env: {
       DATABASE_URL: "postgres://building_passport:password@localhost:65432/building_passport",
       ELCA_LEGACY_DATABASE_URL: "postgres://elca:password@localhost:65433/elca",
