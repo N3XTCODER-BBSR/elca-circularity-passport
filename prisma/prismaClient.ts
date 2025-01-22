fcd4f72dc71f809c086eac54eb85b2e73c54c7e1
Aleksandra Zinoveva <alisenz@icloud.com>
2025-01-22 14:52:25 +0100

    Communicates connection pool size to Prisma client. Also added as an .env variable.

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,15 +1,34 @@
+import { create } from "lodash"
 import { Prisma, PrismaClient } from "./generated/client"
 import { PrismaClient as PrismaLegacyClient } from "./generated/client-legacy"
 
 const options: Prisma.PrismaClientOptions | undefined =
   process.env.NODE_ENV === "development" ? { log: ["query"] } : undefined
 
+const modifyDatabaseUrlWithLimit = (url: string | undefined, limit: number): string => {
+  if (!url) {
+    throw new Error("Database URL is undefined. Please provide a valid connection string.")
+  }
+  const separator = url.includes("?") ? "&" : "?"
+  return `${url}${separator}connection_limit=${limit}`
+}
+
 const prismaClientSingleton = () => {
-  return new PrismaClient({ ...options, datasourceUrl: process.env.DATABASE_URL })
+  const url = modifyDatabaseUrlWithLimit(
+    process.env.DATABASE_URL,
+    process.env.POOL_MAX_CONN ? parseInt(process.env.POOL_MAX_CONN) : 5
+  )
+  console.log(`Connecting to database: ${url}`)
+  return new PrismaClient({ ...options, datasourceUrl: url })
 }
 
 const prismaLegacyClientSingleton = () => {
-  return new PrismaLegacyClient({ ...options, datasourceUrl: process.env.ELCA_LEGACY_DATABASE_URL })
+  const url = modifyDatabaseUrlWithLimit(
+    process.env.ELCA_LEGACY_DATABASE_URL,
+    process.env.POOL_MAX_CONN ? parseInt(process.env.POOL_MAX_CONN) : 5
+  )
+  console.log(`Connecting to database: ${url}`)
+  return new PrismaLegacyClient({ ...options, datasourceUrl: url })
 }
 
 const prismaLegacySuperUserClientSingleton = () => {
