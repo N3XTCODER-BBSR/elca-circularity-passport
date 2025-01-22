1b37d511202bc5e630fb0233b5e5eec08516bd3b
Aleksandra Zinoveva <alisenz@icloud.com>
2025-01-22 16:56:29 +0100

    Comments in response to PR request: use URL module and add pool timeout

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,37 +1,50 @@
 import { Prisma, PrismaClient } from "./generated/client"
 import { PrismaClient as PrismaLegacyClient } from "./generated/client-legacy"
 
+const modifyDatabaseUrl = (urlString: string | undefined): string | undefined => {
+  if (!urlString) return undefined
+
+  try {
+    const url = new URL(urlString)
+    const searchParams = url.searchParams
+
+    if (process.env.DATABASE_POOL_MAX_CONN) {
+      searchParams.set("connection_limit", process.env.DATABASE_POOL_MAX_CONN)
+    }
+    if (process.env.DATABASE_POOL_TIMEOUT) {
+      searchParams.set("pool_timeout", process.env.DATABASE_POOL_TIMEOUT)
+    }
+
+    url.search = searchParams.toString()
+
+    return url.toString()
+  } catch (error) {
+    console.error("Error modifying database URL:", error)
+    return undefined
+  }
+}
+
 const options: Prisma.PrismaClientOptions | undefined =
   process.env.NODE_ENV === "development" ? { log: ["query"] } : undefined
 
-const modifyDatabaseUrlWithLimit = (url: string | undefined, limit: number): string => {
-  if (!url) {
-    throw new Error("Database URL is undefined. Please provide a valid connection string.")
-  }
-  const separator = url.includes("?") ? "&" : "?"
-  return `${url}${separator}connection_limit=${limit}`
-}
-
 const prismaClientSingleton = () => {
-  const url = modifyDatabaseUrlWithLimit(
-    process.env.DATABASE_URL,
-    process.env.POOL_MAX_CONN ? parseInt(process.env.POOL_MAX_CONN) : 5
-  )
+  const url = modifyDatabaseUrl(process.env.DATABASE_URL)
+  console.log("url", url)
   return new PrismaClient({ ...options, datasourceUrl: url })
 }
 
 const prismaLegacyClientSingleton = () => {
-  const url = modifyDatabaseUrlWithLimit(
-    process.env.ELCA_LEGACY_DATABASE_URL,
-    process.env.POOL_MAX_CONN ? parseInt(process.env.POOL_MAX_CONN) : 5
-  )
+  const url = modifyDatabaseUrl(process.env.ELCA_LEGACY_DATABASE_URL)
+  console.log("url", url)
   return new PrismaLegacyClient({ ...options, datasourceUrl: url })
 }
 
 const prismaLegacySuperUserClientSingleton = () => {
+  const url = modifyDatabaseUrl(process.env.ELCA_LEGACY_DATABASE_URL_SUPERUSER_FOR_TESTING)
+  console.log("url", url)
   return new PrismaLegacyClient({
     ...options,
-    datasourceUrl: process.env.ELCA_LEGACY_DATABASE_URL_SUPERUSER_FOR_TESTING,
+    datasourceUrl: url,
   })
 }
 
