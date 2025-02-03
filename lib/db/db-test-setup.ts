3f2f5f0a55a59bcf24087fa077f894ab92052d74
Jonathan Moore <dev@n3xtcoder.org>
2025-02-03 15:09:47 +0100

    chore: remove a newline to satisfy prettier

--- lib/db/db-test-setup.ts
+++ lib/db/db-test-setup.ts
@@ -86,7 +86,6 @@ const main = async () => {
     process.env.DATABASE_POOL_TIMEOUT = "20000"
     process.env.LEGACY_DATABASE_POOL_MAX_CONN = "2"
     process.env.LEGACY_DATABASE_POOL_TIMEOUT = "20000"
-
     ;(globalThis as unknown as { [key: string]: StartedTestContainer }).__PASSPORT_DB_CONTAINER__ = passportDbContainer
     ;(globalThis as unknown as { [key: string]: StartedTestContainer }).__ELCA_DB_CONTAINER__ = elcaDbContainer
   } catch (error) {
