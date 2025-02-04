38d610724fe880da0bf9a3131771273d4ce54690
Jonathan Moore <dev@n3xtcoder.org>
2025-02-04 13:54:10 +0100

    chore: remove config defaults and rollback pool config for the test db superuser

--- prisma/prismaClient.ts
+++ prisma/prismaClient.ts
@@ -1,18 +1,15 @@
 import { Prisma, PrismaClient } from "./generated/client"
 import { PrismaClient as PrismaLegacyClient } from "./generated/client-legacy"
 
-const DATABASE_POOL_MAX_CONN = process.env.DATABASE_POOL_MAX_CONN || 2
-const DATABASE_POOL_TIMEOUT = process.env.DATABASE_POOL_TIMEOUT || 20000
-const DATABASE_URL =
-  process.env.DATABASE_URL || "postgresql://building_passport:password@localhost:65432/building_passport"
-const LEGACY_DATABASE_POOL_MAX_CONN = process.env.LEGACY_DATABASE_POOL_MAX_CONN || 2
-const LEGACY_DATABASE_POOL_TIMEOUT = process.env.LEGACY_DATABASE_POOL_TIMEOUT || 20000
-const LEGACY_DATABASE_URL =
-  process.env.ELCA_LEGACY_DATABASE_URL || "postgres://elca_read_only:password@localhost:65433/elca"
+const DATABASE_POOL_MAX_CONN = process.env.DATABASE_POOL_MAX_CONN
+const DATABASE_POOL_TIMEOUT = process.env.DATABASE_POOL_TIMEOUT
+const DATABASE_URL = process.env.DATABASE_URL
+const LEGACY_DATABASE_POOL_MAX_CONN = process.env.LEGACY_DATABASE_POOL_MAX_CONN
+const LEGACY_DATABASE_POOL_TIMEOUT = process.env.LEGACY_DATABASE_POOL_TIMEOUT
+const LEGACY_DATABASE_URL = process.env.ELCA_LEGACY_DATABASE_URL
 const LEGACY_DATABASE_SUPERUSER_FOR_TESTING_POOL_MAX_CONN = 1
 const LEGACY_DATABASE_UPERUSER_FOR_TESTING_TIMEOUT = 20000
-const LEGACY_DATABASE_SUPERUSER_FOR_TESTING_URL =
-  process.env.ELCA_LEGACY_DATABASE_URL_SUPERUSER_FOR_TESTING || "postgres://elca:password@localhost:65433/elca"
+const LEGACY_DATABASE_SUPERUSER_FOR_TESTING_URL = process.env.ELCA_LEGACY_DATABASE_URL_SUPERUSER_FOR_TESTING
 
 const modifyDatabaseUrl = (urlString: string, maxConnection: number, timeOut: number): string => {
   try {
@@ -34,24 +31,23 @@ const options: Prisma.PrismaClientOptions | undefined =
   process.env.NODE_ENV === "development" ? { log: ["query"] } : undefined
 
 const prismaClientSingleton = () => {
-  const url = modifyDatabaseUrl(DATABASE_URL, DATABASE_POOL_MAX_CONN, DATABASE_POOL_TIMEOUT)
+  const url = modifyDatabaseUrl(DATABASE_URL as string, Number(DATABASE_POOL_MAX_CONN), Number(DATABASE_POOL_TIMEOUT))
   return new PrismaClient({ ...options, datasourceUrl: url })
 }
 
 const prismaLegacyClientSingleton = () => {
-  const url = modifyDatabaseUrl(LEGACY_DATABASE_URL, LEGACY_DATABASE_POOL_MAX_CONN, LEGACY_DATABASE_POOL_TIMEOUT)
+  const url = modifyDatabaseUrl(
+    LEGACY_DATABASE_URL as string,
+    Number(LEGACY_DATABASE_POOL_MAX_CONN),
+    Number(LEGACY_DATABASE_POOL_TIMEOUT)
+  )
   return new PrismaLegacyClient({ ...options, datasourceUrl: url })
 }
 
 const prismaLegacySuperUserClientSingleton = () => {
-  const url = modifyDatabaseUrl(
-    LEGACY_DATABASE_SUPERUSER_FOR_TESTING_URL,
-    LEGACY_DATABASE_SUPERUSER_FOR_TESTING_POOL_MAX_CONN,
-    LEGACY_DATABASE_UPERUSER_FOR_TESTING_TIMEOUT
-  )
   return new PrismaLegacyClient({
     ...options,
-    datasourceUrl: url,
+    datasourceUrl: process.env.ELCA_LEGACY_DATABASE_URL_SUPERUSER_FOR_TESTING,
   })
 }
 
