4ea6a3692809816541c4ec80db99d0ae150f1d1e
nheidner <niko.heidner@gmail.com>
2025-03-12 16:40:11 +0100

    feat: add tests for getTotalWeightedCircularityPotential and getTotalWeightedDismantlingPotential functions

--- app/[locale]/(circularity)/projects/[projectId]/variants/[variantId]/catalog/components/[componentUuid]/utils/getTotalWeightedCircularityPotential.test.ts
+++ app/[locale]/(circularity)/projects/[projectId]/variants/[variantId]/catalog/components/[componentUuid]/utils/getTotalWeightedCircularityPotential.test.ts
@@ -1,6 +1,45 @@
-// TODO after domain functions refactor: Add test cases
+import { EnrichedElcaElementComponent } from "lib/domain-logic/types/domain-types"
+import { getTotalWeightedCircularityPotential } from "./getTotalWeightedCircularityPotential"
+import { layers } from "./testFixtures"
+
 describe("getTotalWeightedCircularityPotential", () => {
-  it("should ", () => {
-    expect(true).toBeTruthy()
+  test("returns weighted average for valid layers", () => {
+    const result = getTotalWeightedCircularityPotential(layers)
+
+    expect(result).toBeCloseTo(-10.24, 2)
+  })
+
+  test("returns data for layer 1 when layer 2 is excluded from calculation", () => {
+    const result = getTotalWeightedCircularityPotential([
+      { ...layers[0] } as EnrichedElcaElementComponent,
+      { ...layers[1], isExcluded: true } as EnrichedElcaElementComponent,
+    ])
+
+    expect(result).toBe(-60)
+  })
+
+  test("returns null for empty layers", () => {
+    const result = getTotalWeightedCircularityPotential([])
+
+    expect(result).toBeNull()
+  })
+
+  test("returns null for layers with no volume", () => {
+    const result = getTotalWeightedCircularityPotential([
+      { ...layers[0], volume: null } as EnrichedElcaElementComponent,
+    ])
+
+    expect(result).toBeNull()
+  })
+
+  test("returns null for layers with no circularity potential", () => {
+    const result = getTotalWeightedCircularityPotential([
+      {
+        ...layers[0],
+        tBaustoffProductData: { ...layers[0]?.tBaustoffProductData, eolData: {} },
+      } as EnrichedElcaElementComponent,
+    ])
+
+    expect(result).toBeNull()
   })
 })
--- app/[locale]/(circularity)/projects/[projectId]/variants/[variantId]/catalog/components/[componentUuid]/utils/getTotalWeightedDismantlingPotential.test.ts
+++ app/[locale]/(circularity)/projects/[projectId]/variants/[variantId]/catalog/components/[componentUuid]/utils/getTotalWeightedDismantlingPotential.test.ts
@@ -1,6 +1,49 @@
-// TODO after domain functions refactor: Add test cases
-describe("getTotalWeightedCircularityPotential", () => {
-  it("should ", () => {
-    expect(true).toBeTruthy()
+import { dismantlingPotentialClassIdMapping } from "lib/domain-logic/circularity/utils/circularityMappings"
+import { EnrichedElcaElementComponent } from "lib/domain-logic/types/domain-types"
+import { DismantlingPotentialClassId } from "prisma/generated/client"
+import { getTotalWeightedDismantlingPotential } from "./getTotalWeightedDismantlingPotential"
+import { layers } from "./testFixtures"
+
+describe("getTotalWeightedDismantlingPotential", () => {
+  test("returns weighted average for valid layers", () => {
+    const result = getTotalWeightedDismantlingPotential(layers)
+
+    expect(result).toBe(100)
+  })
+
+  test("returns data for layer 1 when layer 2 is excluded from calculation", () => {
+    const dismantlingPotentialClassIdLayer1 = DismantlingPotentialClassId.II
+
+    const result = getTotalWeightedDismantlingPotential([
+      { ...layers[0], dismantlingPotentialClassId: dismantlingPotentialClassIdLayer1 } as EnrichedElcaElementComponent,
+      { ...layers[1], isExcluded: true } as EnrichedElcaElementComponent,
+    ])
+
+    expect(result).toBe(dismantlingPotentialClassIdMapping[dismantlingPotentialClassIdLayer1].points)
+  })
+
+  test("returns null for empty layers", () => {
+    const result = getTotalWeightedDismantlingPotential([])
+
+    expect(result).toBeNull()
+  })
+
+  test("returns null for layers with no volume", () => {
+    const result = getTotalWeightedDismantlingPotential([
+      { ...layers[0], volume: null } as EnrichedElcaElementComponent,
+    ])
+
+    expect(result).toBeNull()
+  })
+
+  test("returns null for layers when dismantlingPotentialClassId is null", () => {
+    const result = getTotalWeightedDismantlingPotential([
+      {
+        ...layers[0],
+        dismantlingPotentialClassId: null,
+      } as EnrichedElcaElementComponent,
+    ])
+
+    expect(result).toBeNull()
   })
 })
