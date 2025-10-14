986ace2edeac50276a41f42484330c663f63f4e3
Daniel Spaude <daniel@n3xtcoder.org>
2025-10-14 18:08:14 +0200

    feat: tBaustoff API - update DB structure - WIP - migration for dropping category indexes

---
+++ prisma/migrations/20251014160620_drop_category_indexes/migration.sql
@@ -0,0 +1,8 @@
+-- DropIndex
+DROP INDEX "TBs_ProductDefinition_uuid_key";
+
+-- DropIndex
+DROP INDEX "TBs_ProductDefinitionEOLCategory_name_key";
+
+-- DropIndex
+DROP INDEX "TBs_ProductDefinitionEOLCategory_uuid_key";
